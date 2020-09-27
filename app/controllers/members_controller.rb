7495ff57ff6984cfdd4460fa7b406e1563fd477d
masamichinakano <ma5sa9mi0chi9@gmail.com>
2020-09-27 12:14:10 +0900

    選手編集削除機能

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,4 +1,6 @@
 class MembersController < ApplicationController
+  before_action :move_to_new_user_session, except: [:index]    
+
 
   def index
   end
@@ -29,11 +31,37 @@ class MembersController < ApplicationController
     @member = Member.find(params[:id])
   end
 
+  def edit
+    @member = Member.find(params[:id])
+  end
+
+  def update
+    # binding.pry
+    @member = Member.find(params[:id])
+    if @member.update(member_params)
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @member = Member.find(params[:id])
+    if @member.destroy
+      redirect_to root_path
+    else
+      render 'show'
+    end
+  end
 
 
   private
   def member_params
     params.require(:member).permit(:image, :name, :age, :style_id, :number, :pitcher_position_id, :catcher_position_id, :inside_position_id, :outside_position_id, :user_id).merge(user_id: current_user.id)
+  end
+
+  def move_to_new_user_session
+    redirect_to new_user_session_path unless user_signed_in?   
   end
 
 
