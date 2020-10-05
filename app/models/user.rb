8590abc0a6a641b42ef1cf7b943f6840174598fd
masamichinakano <ma5sa9mi0chi9@gmail.com>
2020-10-05 20:18:30 +0900

    devise修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,9 @@ class User < ApplicationRecord
   has_many :matchs
   has_many :fronts
 
-  validates :name, presence: true
 
   with_options presence: true do
     validates :name, :email, :password, uniqueness: true
-    validates :email, format: { with: /\A\S+@\S+\.\S+\z/}
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i}
   end
 
 end
