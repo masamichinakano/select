7c36dcf89c0c8a17865535428aa4b0b20206ea81
masamichinakano <ma5sa9mi0chi9@gmail.com>
2020-10-09 19:23:43 +0900

    commit

--- app/javascript/back_first.js
+++ app/javascript/back_first.js
@@ -3,162 +3,162 @@
 
 // 一回表
 window.addEventListener("load", function() {
-  var score = document.getElementById("backs-first-fa-score-field");
-  score.addEventListener("input", function() {
-    var first_fa_score = parseInt(score.value);
-    var fa_total = document.getElementById("backs-fa-total");
+  var b_score = document.getElementById("backs-first-fa-score-field");
+  b_score.addEventListener("input", function() {
+    var b_first_fa_score = parseInt(b_score.value);
+    var b_fa_total = document.getElementById("backs-fa-total");
 
-    fa_total.innerHTML = first_fa_score;
+    b_fa_total.innerHTML = b_first_fa_score;
   })  
   // 二回表
-  var second_score = document.getElementById("backs-second-fa-score-field");
-  second_score.addEventListener("input", function() {
-    var second_fa_score = parseInt(second_score.value);
-    var score = document.getElementById("backs-first-fa-score-field");
-    var first_fa_score = parseInt(score.value);
-    var fa_total = document.getElementById("backs-fa-total");
+  var b_second_score = document.getElementById("backs-second-fa-score-field");
+  b_second_score.addEventListener("input", function() {
+    var b_second_fa_score = parseInt(b_second_score.value);
+    var b_score = document.getElementById("backs-first-fa-score-field");
+    var b_first_fa_score = parseInt(b_score.value);
+    var b_fa_total = document.getElementById("backs-fa-total");
 
-    second_total_score = (first_fa_score + second_fa_score);
-    fa_total.innerHTML = second_total_score 
+    b_second_total_score = (b_first_fa_score + b_second_fa_score);
+    b_fa_total.innerHTML = b_second_total_score 
   })
   
   // 三回表
-  var third_score = document.getElementById("backs-third-fa-score-field");
-  third_score.addEventListener("input", function() {
-    var third_fa_score = parseInt(third_score.value);
-    var score = document.getElementById("backs-first-fa-score-field");
-    var first_fa_score = parseInt(score.value);
-    var second_score = document.getElementById("backs-second-fa-score-field");
-    var second_fa_score = parseInt(second_score.value);
-    var fa_total = document.getElementById("backs-fa-total");
+  var b_third_score = document.getElementById("backs-third-fa-score-field");
+  b_third_score.addEventListener("input", function() {
+    var b_third_fa_score = parseInt(b_third_score.value);
+    var b_score = document.getElementById("backs-first-fa-score-field");
+    var b_first_fa_score = parseInt(b_score.value);
+    var b_second_score = document.getElementById("backs-second-fa-score-field");
+    var b_second_fa_score = parseInt(b_second_score.value);
+    var b_fa_total = document.getElementById("backs-fa-total");
 
-    third_total_score = (first_fa_score + second_fa_score + third_fa_score)
-    fa_total.innerHTML = third_total_score 
+    b_third_total_score = (b_first_fa_score + b_second_fa_score + b_third_fa_score)
+    b_fa_total.innerHTML = b_third_total_score 
     
   })
   // 四回表
-  var fourth_score = document.getElementById("backs-fourth-fa-score-field");
-  fourth_score.addEventListener("input", function() {
-    var fourth_fa_score = parseInt(fourth_score.value);
-    var score = document.getElementById("backs-first-fa-score-field");
-    var first_fa_score = parseInt(score.value);
-    var second_score = document.getElementById("backs-second-fa-score-field");
-    var second_fa_score = parseInt(second_score.value);
-    var third_score = document.getElementById("backs-third-fa-score-field");
-    var third_fa_score = parseInt(third_score.value);
-    var fa_total = document.getElementById("backs-fa-total");
+  var b_fourth_score = document.getElementById("backs-fourth-fa-score-field");
+  b_fourth_score.addEventListener("input", function() {
+    var b_fourth_fa_score = parseInt(b_fourth_score.value);
+    var b_score = document.getElementById("backs-first-fa-score-field");
+    var b_first_fa_score = parseInt(b_score.value);
+    var b_second_score = document.getElementById("backs-second-fa-score-field");
+    var b_second_fa_score = parseInt(b_second_score.value);
+    var b_third_score = document.getElementById("backs-third-fa-score-field");
+    var b_third_fa_score = parseInt(b_third_score.value);
+    var b_fa_total = document.getElementById("backs-fa-total");
 
-    fourth_total_score = (first_fa_score + second_fa_score + third_fa_score + fourth_fa_score)
-    fa_total.innerHTML = fourth_total_score 
+    b_fourth_total_score = (b_first_fa_score + b_second_fa_score + b_third_fa_score + b_fourth_fa_score)
+    b_fa_total.innerHTML = b_fourth_total_score 
   })
 
   // 五回表
-  var fifth_score = document.getElementById("backs-fifth-fa-score-field");
-  fifth_score.addEventListener("input", function() {
-    var fifth_fa_score = parseInt(fifth_score.value);
-    var score = document.getElementById("backs-first-fa-score-field");
-    var first_fa_score = parseInt(score.value);
-    var second_score = document.getElementById("backs-second-fa-score-field");
-    var second_fa_score = parseInt(second_score.value);
-    var third_score = document.getElementById("backs-third-fa-score-field");
-    var third_fa_score = parseInt(third_score.value);
-    var fourth_score = document.getElementById("backs-fourth-fa-score-field");
-    var fourth_fa_score = parseInt(fourth_score.value);
-    var fa_total = document.getElementById("backs-fa-total");
+  var b_fifth_score = document.getElementById("backs-fifth-fa-score-field");
+  b_fifth_score.addEventListener("input", function() {
+    var b_fifth_fa_score = parseInt(b_fifth_score.value);
+    var b_score = document.getElementById("backs-first-fa-score-field");
+    var b_first_fa_score = parseInt(b_score.value);
+    var b_second_score = document.getElementById("backs-second-fa-score-field");
+    var b_second_fa_score = parseInt(b_second_score.value);
+    var b_third_score = document.getElementById("backs-third-fa-score-field");
+    var b_third_fa_score = parseInt(b_third_score.value);
+    var b_fourth_score = document.getElementById("backs-fourth-fa-score-field");
+    var b_fourth_fa_score = parseInt(b_fourth_score.value);
+    var b_fa_total = document.getElementById("backs-fa-total");
 
-    fifth_total_score = (first_fa_score + second_fa_score + third_fa_score + fourth_fa_score + fifth_fa_score)
-    fa_total.innerHTML = fifth_total_score 
+    b_fifth_total_score = (b_first_fa_score + b_second_fa_score + b_third_fa_score + b_fourth_fa_score + b_fifth_fa_score)
+    b_fa_total.innerHTML = b_fifth_total_score 
   })
     
   // 六回表
-  var sixth_score = document.getElementById("backs-sixth-fa-score-field");
-  sixth_score.addEventListener("input", function() {
-    var sixth_fa_score = parseInt(sixth_score.value);
-    var score = document.getElementById("backs-first-fa-score-field");
-    var first_fa_score = parseInt(score.value);
-    var second_score = document.getElementById("backs-second-fa-score-field");
-    var second_fa_score = parseInt(second_score.value);
-    var third_score = document.getElementById("backs-third-fa-score-field");
-    var third_fa_score = parseInt(third_score.value);
-    var fourth_score = document.getElementById("backs-fourth-fa-score-field");
-    var fourth_fa_score = parseInt(fourth_score.value);
-    var fifth_score = document.getElementById("backs-fifth-fa-score-field");
-    var fifth_fa_score = parseInt(fifth_score.value);
-    var fa_total = document.getElementById("backs-fa-total");
+  var b_sixth_score = document.getElementById("backs-sixth-fa-score-field");
+  b_sixth_score.addEventListener("input", function() {
+    var b_sixth_fa_score = parseInt(b_sixth_score.value);
+    var b_score = document.getElementById("backs-first-fa-score-field");
+    var b_first_fa_score = parseInt(b_score.value);
+    var b_second_score = document.getElementById("backs-second-fa-score-field");
+    var b_second_fa_score = parseInt(b_second_score.value);
+    var b_third_score = document.getElementById("backs-third-fa-score-field");
+    var b_third_fa_score = parseInt(b_third_score.value);
+    var b_fourth_score = document.getElementById("backs-fourth-fa-score-field");
+    var b_fourth_fa_score = parseInt(b_fourth_score.value);
+    var b_fifth_score = document.getElementById("backs-fifth-fa-score-field");
+    var b_fifth_fa_score = parseInt(b_fifth_score.value);
+    var b_fa_total = document.getElementById("backs-fa-total");
 
-    sixth_total_score = (first_fa_score + second_fa_score + third_fa_score + fourth_fa_score + fifth_fa_score + sixth_fa_score)
-    fa_total.innerHTML = sixth_total_score 
+    b_sixth_total_score = (b_first_fa_score + b_second_fa_score + b_third_fa_score + b_fourth_fa_score + b_fifth_fa_score + b_sixth_fa_score)
+    b_fa_total.innerHTML = b_sixth_total_score 
   })
 
   // 七回表
-  var seventh_score = document.getElementById("backs-seventh-fa-score-field");
-  seventh_score.addEventListener("input", function() {
-    var seventh_fa_score = parseInt(seventh_score.value);
-    var score = document.getElementById("backs-first-fa-score-field");
-    var first_fa_score = parseInt(score.value);
-    var second_score = document.getElementById("backs-second-fa-score-field");
-    var second_fa_score = parseInt(second_score.value);
-    var third_score = document.getElementById("backs-third-fa-score-field");
-    var third_fa_score = parseInt(third_score.value);
-    var fourth_score = document.getElementById("backs-fourth-fa-score-field");
-    var fourth_fa_score = parseInt(fourth_score.value);
-    var fifth_score = document.getElementById("backs-fifth-fa-score-field");
-    var fifth_fa_score = parseInt(fifth_score.value);
-    var sixth_score = document.getElementById("backs-sixth-fa-score-field");
-    var sixth_fa_score = parseInt(sixth_score.value);
-    var fa_total = document.getElementById("backs-fa-total");
+  var b_seventh_score = document.getElementById("backs-seventh-fa-score-field");
+  b_seventh_score.addEventListener("input", function() {
+    var b_seventh_fa_score = parseInt(b_seventh_score.value);
+    var b_score = document.getElementById("backs-first-fa-score-field");
+    var b_first_fa_score = parseInt(b_score.value);
+    var b_second_score = document.getElementById("backs-second-fa-score-field");
+    var b_second_fa_score = parseInt(b_second_score.value);
+    var b_third_score = document.getElementById("backs-third-fa-score-field");
+    var b_third_fa_score = parseInt(b_third_score.value);
+    var b_fourth_score = document.getElementById("backs-fourth-fa-score-field");
+    var b_fourth_fa_score = parseInt(b_fourth_score.value);
+    var b_fifth_score = document.getElementById("backs-fifth-fa-score-field");
+    var b_fifth_fa_score = parseInt(b_fifth_score.value);
+    var b_sixth_score = document.getElementById("backs-sixth-fa-score-field");
+    var b_sixth_fa_score = parseInt(b_sixth_score.value);
+    var b_fa_total = document.getElementById("backs-fa-total");
   
-    seventh_total_score = (first_fa_score + second_fa_score + third_fa_score + fourth_fa_score + fifth_fa_score + sixth_fa_score + seventh_fa_score)
-    fa_total.innerHTML = seventh_total_score 
+    b_seventh_total_score = (b_first_fa_score + b_second_fa_score + b_third_fa_score + b_fourth_fa_score + b_fifth_fa_score + b_sixth_fa_score + b_seventh_fa_score)
+    b_fa_total.innerHTML = b_seventh_total_score 
   })
 
   // 八回表
-  var eighth_score = document.getElementById("backs-eighth-fa-score-field");
-  eighth_score.addEventListener("input", function() {
-    var eighth_fa_score = parseInt(eighth_score.value);
-    var score = document.getElementById("backs-first-fa-score-field");
-    var first_fa_score = parseInt(score.value);
-    var second_score = document.getElementById("backs-second-fa-score-field");
-    var second_fa_score = parseInt(second_score.value);
-    var third_score = document.getElementById("backs-third-fa-score-field");
-    var third_fa_score = parseInt(third_score.value);
-    var fourth_score = document.getElementById("backs-fourth-fa-score-field");
-    var fourth_fa_score = parseInt(fourth_score.value);
-    var fifth_score = document.getElementById("backs-fifth-fa-score-field");
-    var fifth_fa_score = parseInt(fifth_score.value);
-    var sixth_score = document.getElementById("backs-sixth-fa-score-field");
-    var sixth_fa_score = parseInt(sixth_score.value);
-    var seventh_score = document.getElementById("backs-seventh-fa-score-field");
-    var seventh_fa_score = parseInt(seventh_score.value);
-    var fa_total = document.getElementById("backs-fa-total");
+  var b_eighth_score = document.getElementById("backs-eighth-fa-score-field");
+  b_eighth_score.addEventListener("input", function() {
+    var b_eighth_fa_score = parseInt(b_eighth_score.value);
+    var b_score = document.getElementById("backs-first-fa-score-field");
+    var b_first_fa_score = parseInt(b_score.value);
+    var b_second_score = document.getElementById("backs-second-fa-score-field");
+    var b_second_fa_score = parseInt(b_second_score.value);
+    var b_third_score = document.getElementById("backs-third-fa-score-field");
+    var b_third_fa_score = parseInt(b_third_score.value);
+    var b_fourth_score = document.getElementById("backs-fourth-fa-score-field");
+    var b_fourth_fa_score = parseInt(b_fourth_score.value);
+    var b_fifth_score = document.getElementById("backs-fifth-fa-score-field");
+    var b_fifth_fa_score = parseInt(b_fifth_score.value);
+    var b_sixth_score = document.getElementById("backs-sixth-fa-score-field");
+    var b_sixth_fa_score = parseInt(b_sixth_score.value);
+    var b_seventh_score = document.getElementById("backs-seventh-fa-score-field");
+    var b_seventh_fa_score = parseInt(b_seventh_score.value);
+    var b_fa_total = document.getElementById("backs-fa-total");
   
-    eighth_total_score = (first_fa_score + second_fa_score + third_fa_score + fourth_fa_score + fifth_fa_score + sixth_fa_score + seventh_fa_score + eighth_fa_score)
-    fa_total.innerHTML = eighth_total_score
+    b_eighth_total_score = (b_first_fa_score + b_second_fa_score + b_third_fa_score + b_fourth_fa_score + b_fifth_fa_score + b_sixth_fa_score + b_seventh_fa_score + b_eighth_fa_score)
+    b_fa_total.innerHTML = b_eighth_total_score
   })
 
-  // 八回表
-  var ninth_score = document.getElementById("backs-ninth-fa-score-field");
-  ninth_score.addEventListener("input", function() {
-    var ninth_fa_score = parseInt(ninth_score.value);
-    var score = document.getElementById("backs-first-fa-score-field");
-    var first_fa_score = parseInt(score.value);
-    var second_score = document.getElementById("backs-second-fa-score-field");
-    var second_fa_score = parseInt(second_score.value);
-    var third_score = document.getElementById("backs-third-fa-score-field");
-    var third_fa_score = parseInt(third_score.value);
-    var fourth_score = document.getElementById("backs-fourth-fa-score-field");
-    var fourth_fa_score = parseInt(fourth_score.value);
-    var fifth_score = document.getElementById("backs-fifth-fa-score-field");
-    var fifth_fa_score = parseInt(fifth_score.value);
-    var sixth_score = document.getElementById("backs-sixth-fa-score-field");
-    var sixth_fa_score = parseInt(sixth_score.value);
-    var seventh_score = document.getElementById("backs-seventh-fa-score-field");
-    var seventh_fa_score = parseInt(seventh_score.value);
-    var eighth_score = document.getElementById("backs-eighth-fa-score-field");
-    var eighth_fa_score = parseInt(eighth_score.value);
-    var fa_total = document.getElementById("backs-fa-total");
+  // 九回表
+  var b_ninth_score = document.getElementById("backs-ninth-fa-score-field");
+  b_ninth_score.addEventListener("input", function() {
+    var b_ninth_fa_score = parseInt(b_ninth_score.value);
+    var b_score = document.getElementById("backs-first-fa-score-field");
+    var b_first_fa_score = parseInt(b_score.value);
+    var b_second_score = document.getElementById("backs-second-fa-score-field");
+    var b_second_fa_score = parseInt(b_second_score.value);
+    var b_third_score = document.getElementById("backs-third-fa-score-field");
+    var b_third_fa_score = parseInt(b_third_score.value);
+    var b_fourth_score = document.getElementById("backs-fourth-fa-score-field");
+    var b_fourth_fa_score = parseInt(b_fourth_score.value);
+    var b_fifth_score = document.getElementById("backs-fifth-fa-score-field");
+    var b_fifth_fa_score = parseInt(b_fifth_score.value);
+    var b_sixth_score = document.getElementById("backs-sixth-fa-score-field");
+    var b_sixth_fa_score = parseInt(b_sixth_score.value);
+    var b_seventh_score = document.getElementById("backs-seventh-fa-score-field");
+    var b_seventh_fa_score = parseInt(b_seventh_score.value);
+    var b_eighth_score = document.getElementById("backs-eighth-fa-score-field");
+    var b_eighth_fa_score = parseInt(b_eighth_score.value);
+    var b_fa_total = document.getElementById("backs-fa-total");
     
-    ninth_total_score = (first_fa_score + second_fa_score + third_fa_score + fourth_fa_score + fifth_fa_score + sixth_fa_score + seventh_fa_score + eighth_fa_score + ninth_fa_score)
-    fa_total.innerHTML = ninth_total_score 
+    b_ninth_total_score = (b_first_fa_score + b_second_fa_score + b_third_fa_score + b_fourth_fa_score + b_fifth_fa_score + b_sixth_fa_score + b_seventh_fa_score + b_eighth_fa_score + b_ninth_fa_score)
+    b_fa_total.innerHTML = b_ninth_total_score 
   })
 })
