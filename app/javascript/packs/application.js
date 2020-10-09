b4525cdf4e0596288e5afdddf0b87579e26fb30a
masamichinakano <ma5sa9mi0chi9@gmail.com>
2020-10-09 18:11:18 +0900

    修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,11 +12,6 @@ require("../front_second")
 require("../back_first")
 require("../back_second")
 
-// require ("jquery3")
-// require ("popper")
-// require ("bootstrap-sprockets")
-
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
