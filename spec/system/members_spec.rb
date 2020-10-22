2840ea3c7f03104353301bd61ac315f4b78b7ab4
masamichinakano <ma5sa9mi0chi9@gmail.com>
2020-10-22 16:24:23 +0900

    member登録　できない時

--- spec/system/members_spec.rb
+++ spec/system/members_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "Members", type: :system do
     @member = FactoryBot.build(:member)
   end
   context '選手登録ができるとき' do
-    it 'ログインしたユーザーは新規投稿できる' do
+    it '選手登録' do
       # ログインする
       visit root_path
       expect(page).to have_content('ログイン')
@@ -15,9 +15,9 @@ RSpec.describe "Members", type: :system do
       fill_in 'user_password', with: @user.password
       find('input[name="commit"]').click
       expect(current_path).to eq root_path
-      # 新規投稿ページへのリンクがあることを確認する
+      # 選手登録ページへのリンクがあることを確認する
       expect(page).to have_content('選手登録')
-      # 投稿ページに移動する
+      # 登録ページに移動する
       visit new_member_path
       # フォームに情報を入力する
       fill_in "member_name", with: @member.name
@@ -36,10 +36,40 @@ RSpec.describe "Members", type: :system do
       expect(page).to have_content("選手を登録しました。")
     end
   end
-  context 'ツイート投稿ができないとき'do
-    it 'ログインしていないと新規投稿ページに遷移できない' do
-      # トップページに遷移する
-      # 新規投稿ページへのリンクがない
+end
+
+RSpec.describe "Members", type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+    @member = FactoryBot.build(:member)
+  end
+  context '選手登録ができないとき'do
+    it '選手登録' do
+      # ログインする
+      visit root_path
+      expect(page).to have_content('ログイン')
+      visit new_user_session_path
+      fill_in 'user_email', with: @user.email
+      fill_in 'user_password', with: @user.password
+      find('input[name="commit"]').click
+      expect(current_path).to eq root_path
+      # 選手登録ページへのリンクがあることを確認する
+      expect(page).to have_content('選手登録')
+      # 登録ページに移動する
+      visit new_member_path
+      # フォームに情報を入力する
+      fill_in "member_name", with: " "
+      fill_in "member_age", with: " "
+      select "選択してください", from: "member_hitting"
+      fill_in "member_number", with: " "
+      check "member_outside_position_id"
+      attach_file("member_image", "public/images/test_image.png")
+      # 登録を押してもユーザーモデルのカウントは上がらないことを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { Member.count }.by(0)
+      # 新規登録ページへ戻されることを確認する
+      expect(current_path).to eq "/members"
     end
   end
 end
