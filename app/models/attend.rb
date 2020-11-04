f7e55aa531939534b2bf7f7a390fd2fef4a96d84
masamichinakano <ma5sa9mi0chi9@gmail.com>
2020-11-04 16:52:47 +0900

    commit

--- app/models/attend.rb
+++ app/models/attend.rb
@@ -1,7 +1,7 @@
 class Attend < ApplicationRecord
   belongs_to :user
   belongs_to :schedule
-
-  validates :attend_name_id, presence: true
-
+  with_options presence: true do
+    validates :attend_name_id, numericality: {only_integer: true}
+  end
 end
